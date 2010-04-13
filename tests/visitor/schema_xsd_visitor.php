5ef16750aad009d5a19a4f82d709e7430b952817
Kore Nordmann <mail@kore-nordmann.de>
2010-04-13 09:16:53 +0000

    - Tested: More complex XSD schema visiting

--- tests/visitor/schema_xsd_visitor.php
+++ tests/visitor/schema_xsd_visitor.php
@@ -53,6 +53,22 @@ class slVisitorSchemaXmlSchemaTests extends PHPUnit_Framework_TestCase
                 ),
                 'simple_2'
             ),
+            array(
+                array(
+                    __DIR__ . '/../main/data/simple.xml',
+                    __DIR__ . '/../main/data/simple_2.xml',
+                    __DIR__ . '/../main/data/simple_3.xml',
+                ),
+                'simple_3'
+            ),
+            array(
+                array(
+                    __DIR__ . '/../main/data/simple.xml',
+                    __DIR__ . '/../main/data/simple_2.xml',
+                    __DIR__ . '/../main/data/simple_4.xml',
+                ),
+                'simple_4'
+            ),
         );
     }
 
