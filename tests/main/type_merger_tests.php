b34f88a6e898f81c80f7ff0b3a9b1bb0ef180f7c
Kore Nordmann <mail@kore-nordmann.de>
2010-04-14 12:43:38 +0000

    - Tested: More complex case of type merging

--- tests/main/type_merger_tests.php
+++ tests/main/type_merger_tests.php
@@ -146,5 +146,139 @@ class slMainTypeMergerTests extends PHPUnit_Framework_TestCase
             $merger->getTypeMapping()
         );
     }
+
+    public function testMergeRecursiveTypes()
+    {
+        $elements = array(
+            'root_type' => new slSchemaElement(
+                'root',
+                $rootType = new slSchemaType( 'root_type' )
+            ),
+            'a_type' => new slSchemaElement(
+                'a',
+                $aType = new slSchemaType( 'a_type' )
+            ),
+            'b_type' => new slSchemaElement(
+                'b',
+                $bType = new slSchemaType( 'b_type' )
+            ),
+        );
+
+        $rootType->automaton->learn( array( 0, new slSchemaAutomatonNode( 'a', 'a_type' ), 1 ) );
+        $rootType->learnAttributes( array() );
+
+        $aType->automaton->learn( array( 0, new slSchemaAutomatonNode( 'b', 'b_type' ), 1 ) );
+        $aType->learnAttributes( array() );
+
+        $bType->automaton->learn( array( 0, new slSchemaAutomatonNode( 'a', 'a_type' ), 1 ) );
+        $bType->learnAttributes( array() );
+
+        $merger = new slConfigurableTypeMerger(
+            new slSchemaTypeSubsumingPatternComparator(),
+            new slSchemaTypeEqualAttributeComparator()
+        );
+
+        $this->assertEquals(
+            array(
+                'root_type' => new slSchemaElement( 'root', $rootType ),
+                'a_type'    => new slSchemaElement( 'a', $aType ),
+            ),
+            $merger->groupTypes( $elements )
+        );
+
+        $this->assertEquals(
+            array(
+                'b_type' => 'root_type',
+            ),
+            $merger->getTypeMapping()
+        );
+    }
+
+    public function testMergeComplexTypes()
+    {
+        $elements = array(
+            'r_type' => new slSchemaElement(
+                'root',
+                $rType = new slSchemaType( 'r_type' )
+            ),
+            'ra_type' => new slSchemaElement(
+                'a',
+                $raType = new slSchemaType( 'ra_type' )
+            ),
+            'rb_type' => new slSchemaElement(
+                'b',
+                $rbType = new slSchemaType( 'rb_type' )
+            ),
+            'rba_type' => new slSchemaElement(
+                'a',
+                $rbaType = new slSchemaType( 'rba_type' )
+            ),
+            'rbc_type' => new slSchemaElement(
+                'c',
+                $rbcType = new slSchemaType( 'rbc_type' )
+            ),
+            'rbcd_type' => new slSchemaElement(
+                'd',
+                $rbcdType = new slSchemaType( 'rbcd_type' )
+            ),
+            'rc_type' => new slSchemaElement(
+                'c',
+                $rcType = new slSchemaType( 'g_type' )
+            ),
+            'rca_type' => new slSchemaElement(
+                'a',
+                $rcaType = new slSchemaType( 'rca_type' )
+            ),
+        );
+
+        $rType->automaton->learn( array( 0, new slSchemaAutomatonNode( 'a', 'ra_type' ), new slSchemaAutomatonNode( 'b', 'rb_type' ), new slSchemaAutomatonNode( 'c', 'rc_type' ), 1 ) );
+        $rType->learnAttributes( array() );
+
+        $raType->automaton->learn( array( 0, 1 ) );
+        $raType->learnAttributes( array() );
+
+        $rbType->automaton->learn( array( 0, new slSchemaAutomatonNode( 'a', 'rba_type' ), new slSchemaAutomatonNode( 'c', 'rbc_type' ), 1 ) );
+        $rbType->learnAttributes( array() );
+
+        $rbaType->automaton->learn( array( 0, 1 ) );
+        $rbaType->learnAttributes( array() );
+
+        $rbcType->automaton->learn( array( 0, new slSchemaAutomatonNode( 'd', 'rbcd_type' ), 1 ) );
+        $rbcType->learnAttributes( array() );
+
+        $rbcdType->automaton->learn( array( 0, 1 ) );
+        $rbcdType->learnAttributes( array() );
+
+        $rcType->automaton->learn( array( 0, new slSchemaAutomatonNode( 'a', 'rca_type' ), 1 ) );
+        $rcType->learnAttributes( array() );
+
+        $rcaType->automaton->learn( array( 0, 1 ) );
+        $rcaType->learnAttributes( array() );
+
+        $merger = new slConfigurableTypeMerger(
+            new slSchemaTypeSubsumingPatternComparator(),
+            new slSchemaTypeEqualAttributeComparator()
+        );
+
+        $this->assertEquals(
+            array(
+                'r_type'   => new slSchemaElement( 'root', $rType ),
+                'ra_type'  => new slSchemaElement( 'a', $raType ),
+                'rb_type'  => new slSchemaElement( 'b', $rbType ),
+                'rbc_type' => new slSchemaElement( 'c', $rbcType ),
+                'rc_type'  => new slSchemaElement( 'c', $rcType ),
+            ),
+            $merger->groupTypes( $elements )
+        );
+
+        $this->assertEquals(
+            array(
+                'rba_type'  => 'ra_type',
+                'rbcd_type' => 'ra_type',
+                'rca_type'  => 'ra_type',
+            ),
+            $merger->getTypeMapping()
+        );
+    }
 }
 
