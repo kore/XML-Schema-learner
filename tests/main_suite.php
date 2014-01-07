61d9fc2cdf9a3fb15fc4fcd797aa07eeee6b5e96
Michael Orlitzky <michael@orlitzky.com>
2014-01-07 14:33:44 -0500

    Replace "SchemaLearner" with XML-Schema-learner everywhere (and rewrap text).

--- tests/main_suite.php
+++ tests/main_suite.php
@@ -2,20 +2,22 @@
 /**
  * Schema learning
  *
- * This file is part of SchemaLearner.
+ * This file is part of XML-Schema-learner.
  *
- * SchemaLearner is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3 of the License.
+ * XML-Schema-learner is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 3 of the
+ * License.
  *
- * SchemaLearner is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * XML-Schema-learner is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with SchemaLearner; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with XML-Schema-learner; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  *
  * @package Core
  * @version $Revision: 1236 $
@@ -73,7 +75,7 @@ class slMainTestSuite extends PHPUnit_Framework_TestSuite
     public function __construct()
     {
         parent::__construct();
-        $this->setName( 'SchemaLearner - Main' );
+        $this->setName( 'XML-Schema-learner - Main' );
 
         $this->addTest( slMainAutomatonTests::suite() );
         $this->addTest( slMainHiddenMarkovModelTests::suite() );
